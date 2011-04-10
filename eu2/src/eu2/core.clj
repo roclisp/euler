8a07b91ec8ef9c201739abe7055bd9edbddaeeb4
Steve Losh <steve@stevelosh.com>
2011-04-10 16:15:51 -0400

    Moar verbosity

--- eu2/src/eu2/core.clj
+++ eu2/src/eu2/core.clj
@@ -5,8 +5,9 @@
     n
     (+ (fib (- n 1))
        (fib (- n 2)))))
-
 (def fib (memoize fib))
 (def fibs (map fib (iterate inc 1)))
 
-(apply + (filter even? (take-while #(< % 4000000) fibs)))
+
+(def fibs-less-than-4000000 (take-while #(< % 4000000) fibs))
+(apply + (filter even? fibs-less-than-4000000))

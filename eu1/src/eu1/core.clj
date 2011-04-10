d62ba377278678605e3658d00c1f228e4e680c64
Steve Losh <steve@stevelosh.com>
2011-04-10 15:10:36 -0400

    Solve project 1.

--- eu1/src/eu1/core.clj
+++ eu1/src/eu1/core.clj
@@ -1,1 +1,10 @@
 (ns eu1.core)
+
+(defn multiple-3-5? [n]
+  (or (zero? (rem n 3))
+      (zero? (rem n 5))))
+
+(def integers (iterate inc 1))
+(apply + (filter multiple-3-5? (take 999 integers)))
+
+;(apply + (filter multiple-3-5? (range 1 1000)))
